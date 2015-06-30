cb70f37877daacd9c453ef1c6f6f24c1a08941df
Thomas Leskin <tleskin@gmail.com>
2015-06-30 00:30:12 -0400

    Another passing test

--- app/arrays.js
+++ app/arrays.js
@@ -11,7 +11,7 @@ exports.arraysAnswers = {
   },
 
   remove : function(arr, item) {
-
+    return _.reject(arr, function(num){ return num == item });
   },
 
   removeWithoutCopy : function(arr, item) {
