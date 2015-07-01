0c2b57193d903b403f0387932a23e0a8b0028195
Thomas Leskin <tleskin@gmail.com>
2015-07-01 09:51:21 -0400

    Finished Async exercises

--- app/async.js
+++ app/async.js
@@ -2,10 +2,18 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.asyncAnswers = {
   async : function(value) {
-
+    return new Promise(function(resolve, reject){
+        resolve(value);
+    });
   },
 
   manipulateRemoteData : function(url) {
-
+    var people_arr = [];
+    return $.get(url).then(function(people){
+      people["people"].map(function(person){
+        people_arr.push(person["name"]);
+      });
+      return people_arr.sort();
+    });
   }
 };
