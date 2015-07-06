927fd4ab679915400d85e56667d0f752d361fc58
Thomas Leskin <tleskin@gmail.com>
2015-07-05 20:54:46 -0600

    Tests passing for numbers.js

--- app/numbers.js
+++ app/numbers.js
@@ -2,18 +2,21 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.numbersAnswers = {
   valueAtBit: function(num, bit) {
-
+    return 1 & (num >> (bit - 1));
   },
 
   base10: function(str) {
-
+    return parseInt(str, 2)
   },
 
   convertToBinary: function(num) {
-
+    newNum = num.toString(2);
+    while (newNum.length != 8)
+      { newNum = "0" + newNum }
+    return newNum;
   },
 
   multiply: function(a, b) {
-
+    return parseFloat((a * b).toPrecision(1));
   }
 };
